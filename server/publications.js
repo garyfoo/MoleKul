c8919e88858a706ab846ef3d9eb200d7e3cf03ed
mirrokle <MKRL@Lexies-MacBook-Pro.local>
2015-10-12 13:00:24 +1000

    more publications channel feed & wishes

--- server/publications.js
+++ server/publications.js
@@ -23,3 +23,66 @@ Meteor.publishComposite('homefeed', function(limit) {
         ]
     };
 });
+
+Meteor.publishComposite('channelfeed', function(channelId) {
+    return {
+        find: function() {
+            return Channels.find({_id: channelId});
+        },
+        children: [
+            {
+                find: function(channel) {
+                    return Wishes.find({ _id: {$in: channel.wishIds }}, {sort: {createdAt: -1} } );
+                },
+                children: [
+                    {
+                        find: function(wish) {
+                            return Meteor.users.find({_id: wish.userId});
+                        }
+                    },
+                    {
+                        find: function(wish) {
+                            return Meteor.users.find({_id: wish.likedUserIds});
+                        }
+                    }
+                ]
+            }
+        ]
+    };
+});
+
+Meteor.publish('wishes', function() {
+    return Wishes.find();
+});
+
+Meteor.publishComposite('wish', function(_id) {
+    return {
+        find: function() {
+            return Wishes.find({_id: _id});
+        },
+        children: [
+            {
+                find: function(wish) {
+                    return Meteor.users.find({_id: wish.userId});
+                }
+            },
+            {
+                find: function(wish) {
+                    return Meteor.users.find({_id: wish.likeIds});
+                }
+            },
+            {
+                find: function(wish) {
+                    return Comments.find({wishId: wish._id});
+                },
+                children: [
+                    {
+                        find: function(comment) {
+                            return Meteor.users.find({_id: comment.userId});
+                        }
+                    }
+                ]
+            }
+        ]
+    };
+});
