dd389ab03493eba9e54b46a05568780e9a24463d
mirrokle <MKRL@Lexies-MacBook-Pro.local>
2015-10-12 15:10:49 +1000

    added search page

---
+++ client/views/app/explore/explore.js
@@ -0,0 +1,40 @@
+
+var options = {
+    keepHistory: 1000 * 60 * 5,
+    localSearch: true
+};
+
+var fields = ['name'];
+
+ChannelSearch = new SearchSource('channels', fields, options);
+//ChannelSearch.search('');
+
+Template.explore.onRendered(function () {
+    ChannelSearch.search('');
+});
+
+Template.explore.helpers({
+    //channels: function () {
+    //    return Channels.find();
+    //},
+    user: function () {
+        if (Meteor.userId()) {
+            return Meteor.user();
+        }
+    },
+    getChannels: function() {
+        return ChannelSearch.getData(true);
+    },
+
+    isLoading: function() {
+        return ChannelSearch.getStatus().loading;
+    }
+});
+
+
+Template.explore.events({
+    'keyup #searchBox': _.throttle(function(e) {
+        var text = $(e.target).val().trim();
+        ChannelSearch.search(text);
+    }, 200)
+});
